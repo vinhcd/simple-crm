56afce48f091e248d3a477d8026574ffa30901e9
hungnt <hungnt@neoscorp.vn>
2020-08-06 09:51:22 +0700

    remove test class

--- public/index.php
+++ public/index.php
@@ -23,8 +23,6 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-require __DIR__.'/../lib/StaticLogger.php';
-
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
