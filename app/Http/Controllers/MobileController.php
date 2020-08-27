dcf165bb8657892d9366e8c33a5245393dbef89d
hungnt <hungnt@neoscorp.vn>
2020-08-27 08:26:43 +0700

    mobile team support: change uuid

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -8,6 +8,6 @@ class MobileController extends Controller
 {
     public function login(Request $request)
     {
-        return redirect()->to('/mobile?uuid=1200ff02-341c-414c-a8db-ecd68ca0ceeb');
+        return redirect()->to('/mobile?uuid=c0b2ebc1-d1ea-4b23-814a-3abc47b04629');
     }
 }
